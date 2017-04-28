0c21457cfe51a79d88b6871d01def4e871f79268
Andrew Sonin <sonin.cel@gmail.com>
2017-04-28 22:09:22 +0300

    Update wolfram.py

--- wolfram.py
+++ wolfram.py
@@ -9,7 +9,7 @@ class Wolfram():
         for i in range(2):
             for j in range(2):
                 for k in range(2):
-                    self.rules[(i, j, k)] = int(arg[p])
+                    self.rules[i, j, k] = int(arg[p])
                     p += 1
 
     def iterate(self, seq):
